7d495fdd1a7625b26ad2c76cf8c408a1f0e8c0c0
Bruce Ng <bng@uievolution>
2016-03-13 23:53:29 -0700

    changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use strict';
+	//THREE JS Implementation
 	Physijs.scripts.worker = '/js/physijs_worker.js';
 	Physijs.scripts.ammo = '/js/ammo.js';
 
-	var renderer, scene, camera, light, ground_material, ground_geometry, ground, render, loader, frontMotor, backMotor;
+	var renderer, scene, camera, light, ground_material, ground_geometry, ground, render, loader, id;
+	var created = false;
 
 	function initScene() {
 		renderer = new THREE.WebGLRenderer( { alpha: true } );
@@ -17,12 +19,13 @@
 		scene.setGravity(new THREE.Vector3( 0, -30, 0 ));
 		
 		camera = new THREE.PerspectiveCamera(
-			35,
+			90,
 			window.innerWidth / window.innerHeight,
 			1,
-			1000
+			750
 		);
-		camera.position.set( 60, 50, 60 );
+		//camera.position.set( 60, 50, 60 );
+		camera.position.set( 30, 18, 30 );
 		camera.lookAt( scene.position );
 		scene.add( camera );
 		
@@ -41,17 +44,64 @@
 		light.shadowMapWidth = light.shadowMapHeight = 2048;
 		light.shadowDarkness = .7;
 		scene.add( light );
+		createGround();
+		render();
+	};
 
+	initScene();
+
+	function render() {
+		id = requestAnimationFrame( render );
+		/*if (created) {
+			for (var i = 0; i < 5; i++) {
+				for ( i = scene.children.length - 1; i >= 0 ; i -- ) {
+					if (scene.children[i].isMotorcycle) {
+						scene.children[i].rotation.y = scene.children[i].rotation.y + 0.3;
+					}
+				}
+			}
+		}*/
+		renderer.render( scene, camera );
+		scene.simulate();
+	};
+
+	$("#search").click(function() {
+		created = true;
+		createMotorCycle();
+	});	
+
+	function createMotorCycle() {
+		removeEntity();
+
+		var speed = document.getElementById("speed").value;
+		var radian = document.getElementById("radian").value;
+		var lean = calculateLeanAngle(speed, radian);
+		var angle = convertToDegrees(lean);
+		var objectLoader = new THREE.ObjectLoader();
+		document.getElementById("lean").innerHTML = Math.round(angle) + "\xB0";
+
+		objectLoader.load("assets/motortest5.json",function ( front ) {
+			front.name = "Motorcycle";
+			front.scale.x = 0.03;
+	        front.scale.y = 0.03;
+	        front.scale.z = 0.03;
+            front.rotation.x = ((-1 * angle * Math.PI) / 180) / 3;
+	        front.rotation.z = ((-1 * angle * Math.PI) / 180);
+	        front.position.set(-5, 22, 10);
+	        front.isMotorcycle = true;
+            scene.add( front );
+            animate(front);
+		});
+	}
+
+	function createGround() {
 		loader = new THREE.TextureLoader();
-		
-		//Ground
-		ground_material = Physijs.createMaterial(
+				ground_material = Physijs.createMaterial(
 			new THREE.MeshLambertMaterial({map: loader.load('assets/road.jpg')}),
 			.8, // high friction
 			.4 // low restitution
 		);
 		ground_geometry = new THREE.PlaneGeometry( 50, 25, 50, 50 );
-
 		ground = new Physijs.HeightfieldMesh(
 			ground_geometry,
 			ground_material,
@@ -59,69 +109,30 @@
 			50,
 			50
 		);
-
-		ground.position.set(-15, 15, 5);
-
+		ground.position.set(-15, 17, 5);
 		ground.rotation.x = Math.PI / -2;
 		ground.rotation.z = Math.PI / -4;
-
 		ground.receiveShadow = true;
 		ground.name = "floor";
-		/*ground.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
-    		// `this` has collided with `other_object` with an impact speed of `relative_velocity` and a rotational force of `relative_rotation` and at normal `contact_normal`			
-			console.log("The Id of the other object is " + other_object);
-		});*/
 		scene.add( ground );
+	}
 
-		calculateLeanAngle(35.6, 114.8);
+	function removeEntity(){
+	    var obj, i;
+        for ( i = scene.children.length - 1; i >= 0 ; i -- ) {
+            obj = scene.children[ i ];
+            if (obj.isMotorcycle) {
+                scene.remove(obj);
 
-		render();
-	};
+            }
+        }
+	}
 
-	function render() {
-		/*player.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
-    		// `this` has collided with `other_object` with an impact speed of `relative_velocity` and a rotational force of `relative_rotation` and at normal `contact_normal`			
-			if (other_object.name === "left" || other_object.name === "right") {
-				setTimeout(function() {
-					scene.remove(player);
-				}, 400);
-				setTimeout(function() {
-					addPlayer();
-				}, 400);
-			} else if (other_object.name === "Finish") {
-				console.log("Finish Game");
-			}
-		});*/
-		//player1.__dirtyPosition = true;
-		//player1.setAngularVelocity(new THREE.Vector3(0, 0, 0));
-	    //player1.setLinearVelocity(new THREE.Vector3(-6, 0, -6));
-		//player.applyCentralImpulse({x: -1, y: null, z: -1});  
-		
-		requestAnimationFrame( render );
-		renderer.render( scene, camera );
-		scene.simulate();
-	};
-
-	$("#search").click(function() {
-		var speed = document.getElementById("speed").value
-		var radian = document.getElementById("radian").value
-		var lean = calculateLeanAngle(speed, radian);
-		var angle = convertToDegrees(lean);
-		var loader = new THREE.ObjectLoader();
-		document.getElementById("lean").innerHTML = Math.round(angle) + "\xB0";
-		
-		loader.load("assets/motor.json",function ( front ) {
- 			front.scale.x = 0.03;
-            front.scale.y = 0.03;
-            front.scale.z = 0.03;	
-            front.rotation.x = (-90 * Math.PI) / 180;
-            front.rotation.y = (1 * angle * Math.PI) / 180;
-            front.rotation.z = (-145 * Math.PI) / 180;
-    		front.position.set(20, 25, 3);
-            scene.add( front );
-		});
-		console.log(scene);
-	});	
+	function animate(scene) {
+		/*for(var i = 0; i < 5; i++) {
+			scene.children[0].rotation.y = scene.children[0].rotation.y + 0.2;
+		}*/
+	}
 
 	function calculateLeanAngle(speed, radian) {
 		var friction = (Math.pow(speed, 2)) / (9.81 * radian);
@@ -135,6 +146,4 @@
 
 	function convertToAngle(angle) {
 		return (angle * Math.PI) / 180;
-	}
-	
-	window.onload = initScene();
+	}    
